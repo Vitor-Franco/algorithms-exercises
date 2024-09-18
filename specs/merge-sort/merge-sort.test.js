4de303560834123e300c5f67fba23c2bc81f9c3e
Vitor Franco <vitoroliveirafranco@gmail.com>
2024-09-18 20:51:14 -0300

    merge sort first version

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -6,14 +6,59 @@
   You'll need to write more than just one function
 */
 
+const merge = (left = [], right = []) => {
+	const sorted = [];
+
+	let index2 = 0;
+	let index1 = 0;
+
+	for (index1; index1 < left.length; index1++) {
+		const numberOne = left[index1];
+
+		for (index2; index2 < right.length; index2++) {
+			const element2 = right[index2];
+
+			if (numberOne < element2) {
+				sorted.push(numberOne);
+				break;
+			}
+
+			sorted.push(element2);
+		}
+
+		if (!sorted.includes(numberOne)) {
+			sorted.push(numberOne);
+		}
+	}
+
+	if (right.length !== index2) {
+		sorted.push(...right.slice(index2));
+	}
+
+	if (left.length !== index1) {
+		sorted.push(...left.slice(index1));
+	}
+
+	console.log("[LOG] ~ sorted:", sorted)
+	return sorted;
+};
+
 const mergeSort = (nums) => {
-  // code goes here
+	const sizeByTwo = Math.floor(nums.length / 2);
+
+	if (sizeByTwo === 0) {
+		return nums;
+	}
+
+	const leftPart = nums.slice(0, sizeByTwo);
+	const rightPart = nums.slice(sizeByTwo);
+	return merge(mergeSort(leftPart), mergeSort(rightPart));
 };
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
-  const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
-  const ans = mergeSort(nums);
-  expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+test("merge sort", () => {
+	const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+	const ans = mergeSort(nums);
+	expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 });
