1b170d6b074f327892a250c583acaeec5ace0d97
Vitor Franco <vitoroliveirafranco@gmail.com>
2024-12-15 15:43:47 -0300

    add breadth-first traversal

--- specs/traversals/breadth-first.test.js
+++ specs/traversals/breadth-first.test.js
@@ -1,58 +1,68 @@
 const breadthFirstTraverse = (queue, array) => {
-  // fill code in here
+	// fill code in here
+
+	do {
+		const firstItem = queue.shift();
+
+		array.push(firstItem.value);
+		firstItem.left && queue.push(firstItem.left);
+		firstItem.right && queue.push(firstItem.right);
+	} while (queue.length > 0);
+
+	return array;
 };
 
 // unit tests
 // do not modify the below code
-describe.skip("breadth-first tree traversal", function () {
-  const answer = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
+describe("breadth-first tree traversal", () => {
+	const answer = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
 
-  const tree = {
-    value: "A",
-    left: {
-      value: "B",
-      left: {
-        value: "D",
-        left: {
-          value: "G",
-          left: null,
-          right: null
-        },
-        right: null
-      },
-      right: {
-        value: "E",
-        left: null,
-        right: {
-          value: "H",
-          left: {
-            value: "K",
-            left: null,
-            right: null
-          }
-        }
-      }
-    },
-    right: {
-      value: "C",
-      left: {
-        value: "F",
-        left: {
-          value: "I",
-          left: null,
-          right: null
-        },
-        right: {
-          value: "J",
-          left: null,
-          right: null
-        }
-      },
-      right: null
-    }
-  };
+	const tree = {
+		value: "A",
+		left: {
+			value: "B",
+			left: {
+				value: "D",
+				left: {
+					value: "G",
+					left: null,
+					right: null,
+				},
+				right: null,
+			},
+			right: {
+				value: "E",
+				left: null,
+				right: {
+					value: "H",
+					left: {
+						value: "K",
+						left: null,
+						right: null,
+					},
+				},
+			},
+		},
+		right: {
+			value: "C",
+			left: {
+				value: "F",
+				left: {
+					value: "I",
+					left: null,
+					right: null,
+				},
+				right: {
+					value: "J",
+					left: null,
+					right: null,
+				},
+			},
+			right: null,
+		},
+	};
 
-  test("breadthFirstTraverse", () => {
-    expect(breadthFirstTraverse([tree], [])).toEqual(answer);
-  });
+	test("breadthFirstTraverse", () => {
+		expect(breadthFirstTraverse([tree], [])).toEqual(answer);
+	});
 });
