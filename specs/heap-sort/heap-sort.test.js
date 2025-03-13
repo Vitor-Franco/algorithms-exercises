ffd6c1f69fa4da667b9fd64352856024c8c47e56
Vitor Franco <vitoroliveirafranco@gmail.com>
2025-03-13 19:07:52 -0300

    add heapsort impl

--- specs/heap-sort/heap-sort.test.js
+++ specs/heap-sort/heap-sort.test.js
@@ -7,22 +7,59 @@
 */
 
 const heapSort = (array) => {
-  // code
-  return array;
+	createMaxHeap(array);
+
+	for (let index = array.length - 1; index > 0; index--) {
+		swapPlace(0, index, array);
+		heapify(array, 0, index);
+	}
+
+	return array;
 };
 
 const createMaxHeap = (array) => {
-  // code
+  const middleToLeftArrayPart = Math.floor(array.length / 2) - 1;
+	
+  for (let i = middleToLeftArrayPart; i >= 0; i--) {
+		heapify(array, i, array.length);
+	}
+
+  return array;
+};
+
+const swapPlace = (index1, index2, array) => {
+	const temp = array[index1];
+	array[index1] = array[index2];
+	array[index2] = temp;
+
+	return array;
 };
 
 const heapify = (array, index, heapSize) => {
-  // code
+	const FORMULA = 2 * index;
+
+	const leftChildIndex = FORMULA + 1;
+	const rightChildIndex = FORMULA + 2;
+  let largestValueIndex = index;
+
+  if (heapSize > leftChildIndex && array[largestValueIndex] < array[leftChildIndex]) {
+    largestValueIndex = leftChildIndex;
+  }
+  
+  if (heapSize > rightChildIndex && array[largestValueIndex] < array[rightChildIndex]) {
+    largestValueIndex = rightChildIndex;
+  }
+
+	if (largestValueIndex !== index) {
+    swapPlace(index, largestValueIndex, array)
+    heapify(array, largestValueIndex, heapSize)
+  }
 };
 
 // unit tests
 // do not modify the below code
-test.skip("heap sort", function () {
-  const nums = [2, 5, 3, 8, 10, 6, 4, 7, 9, 1];
-  heapSort(nums);
-  expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+test("heap sort", () => {
+	const nums = [2, 5, 3, 8, 10, 6, 4, 7, 9, 1];
+	heapSort(nums);
+	expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 });
